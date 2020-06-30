29bdc535873f33e7ff866b329920068a3b3fd942
ngvuthanhnhan <ngvuthanhnhan@gmail.com>
2020-06-30 11:56:25 +0700

    implement enqueue

--- source_code/src/queue.c
+++ source_code/src/queue.c
@@ -8,6 +8,18 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	if (q->size == MAX_QUEUE_SIZE) 
+        return;
+    for (int i = 0; i < q->size; i++) {
+        if (proc->priority <= q->proc[i]->priority) {
+            q->size++;
+            for (int k = q->size - 1; k > i; k--) {
+                q->proc[k] = q->proc[k - 1]; 
+            }
+            q->proc[i] = proc;
+            break;
+        }
+    }
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
